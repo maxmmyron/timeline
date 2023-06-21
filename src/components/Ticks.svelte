277a03b90d97a1314e81357357ae9b3d609fb55c
maxmmyron <41480159+maxmmyron@users.noreply.github.com>
2023-06-21 17:40:35 -0400

    microticks, offset

--- src/components/Ticks.svelte
+++ src/components/Ticks.svelte
@@ -2,12 +2,21 @@
   export let scrollX = 0;
 
   let width = 0;
+
+  $: offset = Math.floor(scrollX / (width / 6));
+
+  const tickHeights = [1, 1, 1, 2, 1, 1];
 </script>
 
 <p>{scrollX % (width / 6)}</p>
 <div class="tick-container" bind:clientWidth={width} style="transform: translateX(-{scrollX % (width / 6)}px)">
   {#each { length: 6 } as _, i}
-    <div class="tick">{i}</div>
+    <div class="tick">
+      <div style="absolute">{i + offset}</div>
+      {#each tickHeights as h}
+        <div class="inner-tick" style="height: {h * 12}px" />
+      {/each}
+    </div>
   {/each}
 </div>
 
@@ -15,16 +24,22 @@
   .tick-container {
     width: 120%;
     height: 48px;
-    background-color: lightblue;
+    background-color: rgba(240 240 240 / 1);
     padding: 6px 0;
     display: flex;
   }
 
   .tick {
+    display: flex;
     width: 20%;
     height: 100%;
-    background-color: white;
-    border-radius: 4px;
-    border: 1px solid black;
+    border-left: 1px solid black;
+  }
+
+  .inner-tick {
+    width: 100%;
+  }
+  .inner-tick:not(:first-child) {
+    border-left: 1px solid black;
   }
 </style>
