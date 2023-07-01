7d10d665977b4ce1dadcb002e522c4ad0edb0a92
maxmmyron <41480159+maxmmyron@users.noreply.github.com>
2023-07-01 08:47:45 -0400

    add test types

--- src/routes/async/+page.svelte
+++ src/routes/async/+page.svelte
@@ -1,35 +1,79 @@
 <script>
   /**
-   * @type {{id: string, data: Promise<{x:number,y:number}>}[]}
+   * @type {{id: string; data: Promise<App.Media>}[]}
    */
   let media = [];
 
   /**
-   * @type {{id: string, mediaID: string}[]}
+   * @type {{id: string; data: App.Media}[]}
+   */
+  let resolved = [];
+
+  /**
+   * @type {{id: string; mediaID: string}[]}
    */
   let timeline = [];
 
   /**
-   * @type {Set<string>}
+   * @type {Map<string, string>}
    */
-  let buffer = new Set();
+  let buffer = new Map();
 
   /**
    * Adds new promise to media
    */
   const addMedia = async () => {
+    let id = Math.random().toString(36).substring(2, 9);
+
     media = [
       ...media,
       {
-        id: Math.random().toString(36).substring(2, 9),
+        id,
         data: new Promise((resolve, reject) =>
           setTimeout(() => {
             if (Math.random() > 0.5) reject("Something went wrong");
-            else
-              resolve({
-                x: Math.random() * 100,
-                y: Math.random() * 100,
-              });
+            else {
+              /**
+               * @type {App.Media | null}
+               */
+              let data = null;
+              switch (Math.floor(Math.random() * 3)) {
+                case 0:
+                  data = {
+                    type: "A",
+                    foo: Math.random().toString(36).substring(2, 9),
+                    bar: Math.random() * 100,
+                  };
+                  break;
+                case 1:
+                  data = {
+                    type: "B",
+                    baz: Math.random().toString(36).substring(2, 9),
+                    qux: Math.random() * 100,
+                  };
+                  break;
+                case 2:
+                  data = {
+                    type: "C",
+                    fizz: Math.random().toString(36).substring(2, 9),
+                    buzz: Math.random() * 100,
+                  };
+                  break;
+                default:
+                  reject("Something went wrong");
+              }
+
+              if (data) {
+                resolved = [
+                  ...resolved,
+                  {
+                    id,
+                    data,
+                  },
+                ];
+                resolve(data);
+              }
+            }
           }, Math.random() * 5000)
         ),
       },
@@ -37,12 +81,13 @@
   };
 
   /**
-   * Removes resolved promise from media based on ID; removes from timeline if present
+   * Removes element from media based on ID
    *
    * @param {string} id
    */
   const removeMedia = (id) => {
     media = media.filter((clip) => clip.id !== id);
+    resolved = resolved.filter((clip) => clip.id !== id);
     timeline = timeline.filter((clip) => clip.mediaID !== id);
   };
 
@@ -60,7 +105,7 @@
       },
     ];
 
-    buffer = buffer.add(mediaID);
+    buffer = buffer.set(mediaID, Math.random().toString(36).substring(2, 9));
   };
 
   /**
@@ -99,14 +144,35 @@
       <div style="display:flex; gap:16px;">
         {#await data}
           <p>loading...</p>
-        {:then { x, y }}
+        {:then data}
           <button style="padding: 8px;" on:click={() => removeMedia(id)}>x</button>
           <button style="padding: 8px;" on:click={() => addTimeline(id)}>+</button>
-          <p>({id}): {x}, {y}</p>
+          {#if data.type === "A"}
+            <p>({id}): {data.type}; {data.foo}, {data.bar}</p>
+          {:else if data.type === "B"}
+            <p>({id}): {data.type}; {data.baz}, {data.qux}</p>
+          {:else if data.type === "C"}
+            <p>({id}): {data.type}; {data.fizz}, {data.buzz}</p>
+          {/if}
         {:catch error}
           <button style="padding: 8px;" on:click={() => removeMedia(id)}>x</button>
           <p>({id}): {error}</p>
         {/await}
+      </div>
+    {/each}
+  </div>
+
+  <div style="width: 100%">
+    <h2>resolved</h2>
+    {#each resolved as { id, data }}
+      <div style="display:flex; gap:16px;">
+        {#if data.type === "A"}
+          <p>({id}): {data.type}; {data.foo}, {data.bar}</p>
+        {:else if data.type === "B"}
+          <p>({id}): {data.type}; {data.baz}, {data.qux}</p>
+        {:else if data.type === "C"}
+          <p>({id}): {data.type}; {data.fizz}, {data.buzz}</p>
+        {/if}
       </div>
     {/each}
   </div>
@@ -116,7 +182,7 @@
     {#each timeline as { id, mediaID }}
       <div style="display:flex; gap:16px;">
         <button style="padding: 8px;" on:click={() => removeTimeline(id)}>x</button>
-        <p>({id}): {mediaID}</p>
+        <p>({id}): {mediaID} ; {resolved.find((m) => m.id === mediaID)?.data.type}</p>
       </div>
     {/each}
   </div>
