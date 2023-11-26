927cbba1e1a206ef36395621dc3603fb9cfc1b90
maxmmyron <41480159+maxmmyron@users.noreply.github.com>
2023-11-25 21:19:29 -0500

    eh

---
+++ src/routes/mover/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+</script>
+
+<div class="timeline"></div>
+
+<style>
+  .timeline {
+    position: relative;
+    height: 100px;
+    width: 100%;
+    background: #eee;
+    overflow-x: scroll;
+  }
+</style>
