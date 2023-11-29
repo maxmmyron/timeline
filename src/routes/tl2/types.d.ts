bd227c4d1170adec9355c7dc1a4564dd487b779a
maxmmyron <41480159+maxmmyron@users.noreply.github.com>
2023-11-29 17:25:21 -0500

    improve types

--- src/routes/tl2/types.d.ts
+++ src/routes/tl2/types.d.ts
@@ -1,11 +1,19 @@
 type MediaType = 'image' | 'video' | 'audio';
 
 type Clip = {
-    file: File;
-    type: MediaType;
-    duration: number;
-    offset: number;
-    start: number;
-    uuid: string;
-    z: number;
-  };
+  media: Media;
+  offset: number;
+  start: number;
+  uuid: string;
+  z: number;
+};
+
+type Media = {
+  uuid: string;
+  src: string;
+  type: MediaType;
+  duration: number;
+  title: string;
+  audio: AudioBuffer | null;
+  thumbnails: string[] | null;
+};
