8af618e4569ad63fa4ed12ccae2ccdccbdf2d613
maxmmyron <41480159+maxmmyron@users.noreply.github.com>
2024-01-05 11:19:32 -0500

    support trimming with sides

--- src/routes/tl3/lib/TimelineClip.svelte
+++ src/routes/tl3/lib/TimelineClip.svelte
@@ -9,15 +9,25 @@
   let clipEl: HTMLButtonElement;
 
   /**
-   * Distance from left of clip to mouse
+   * Distance from left of clip to mouse. Used to calculate new offset and to
+   * maintain position of mouse on clip.
    */
-  let movedOffset = 0;
+  let moveOffset = 0;
+  let initialResizeMousePos = 0;
+
+  /**
+   * Initial values of clip when resizing, so resize calculates don't resize
+   * based on recently updated start/end/offset values
+   */
+  let initialTrimValues = { start: 0, end: 0, offset: 0 };
+
+  $: clipLength = clip.media.duration - clip.start - clip.end;
 
   $: transform = `translateX(${clip.offset * TIME_SCALING}px)`;
-  $: width = `${clip.media.duration * TIME_SCALING}px`;
+  $: width = `${clipLength * TIME_SCALING}px`;
 
   const moveClip = (clientX: number) => {
-    const newPos = clientX - movedOffset;
+    const newPos = clientX - moveOffset;
 
     clip.offset = Math.max(0, newPos / TIME_SCALING);
 
@@ -25,7 +35,22 @@
     $videoClips = [...$videoClips];
   };
 
-  const resizeClip = (clientX: number) => {};
+  const resizeClip = (clientX: number) => {
+    const delta = clientX - initialResizeMousePos;
+
+    if (resizeMode === "left") {
+      clip.start = Math.max(0, initialTrimValues.start + delta / TIME_SCALING);
+      clip.offset = Math.max(
+        0,
+        initialTrimValues.offset + delta / TIME_SCALING
+      );
+    } else if (resizeMode === "right") {
+      clip.end = Math.max(0, initialTrimValues.end - delta / TIME_SCALING);
+    }
+
+    // reassign to trigger reactivity
+    $videoClips = [...$videoClips];
+  };
 </script>
 
 <svelte:window
@@ -47,14 +72,30 @@
   bind:this={clipEl}
   on:mousedown|stopPropagation={(e) => {
     canMoveClip = true;
-    movedOffset = e.clientX - clipEl.getBoundingClientRect().left;
+    moveOffset = e.clientX - clipEl.getBoundingClientRect().left;
   }}
   on:click
 >
-  <button class="trimmer left" on:click|stopPropagation={(e) => {}}></button>
+  <button
+    class="trimmer left"
+    on:mousedown|capture|stopPropagation={(e) => {
+      resizeMode = "left";
+      initialResizeMousePos = e.clientX;
+      initialTrimValues.start = clip.start;
+      initialTrimValues.offset = clip.offset;
+    }}
+  ></button>
   <p>{clip.uuid}, {clip.z}</p>
   <p>{clip.media.title}</p>
-  <button class="trimmer right" on:click|stopPropagation={(e) => {}}></button>
+  <button
+    class="trimmer right"
+    on:mousedown|capture|stopPropagation={(e) => {
+      resizeMode = "right";
+      initialResizeMousePos = e.clientX;
+      initialTrimValues.end = clip.end;
+      initialTrimValues.offset = clip.offset;
+    }}
+  ></button>
 </button>
 
 <style>
