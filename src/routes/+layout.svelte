3006ea8d55f1cb40acf5ae405f9f56f101bc184f
maxmmyron <41480159+maxmmyron@users.noreply.github.com>
2024-01-09 12:25:45 -0500

    use layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,23 @@
 <script lang="ts">
   import { ffmpeg } from "$lib/stores";
   import { onMount } from "svelte";
+  import "../app.css";
 
   onMount(async () => {
     if (!$ffmpeg.isLoaded()) await $ffmpeg.load();
   });
 </script>
 
-<slot />
+<main>
+  <slot />
+</main>
+
+<style>
+  main {
+    height: 100vh;
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-template-rows: 48px 96px 1fr 48px 128px;
+    gap: 0.5rem;
+  }
+</style>
