ea970e3add35f97cb52af47b46bf2b375b0306e4
maxmmyron <41480159+maxmmyron@users.noreply.github.com>
2024-01-13 13:36:51 -0500

    implement scaling in export

--- src/lib/export.ts
+++ src/lib/export.ts
@@ -24,7 +24,7 @@ export const exportVideo = async () => {
   }
 
   // create black video with empty audio track for duration of video
-  await ffmpegInstance.run("-t", duration.toString(), "-f", "lavfi", "-i", "color=c=black:s=1280x720:r=30", "-f", "lavfi", "-i", "anullsrc=channel_layout=stereo:sample_rate=44100", "-pix_fmt", "yuv420p", "-shortest", "base.mp4");
+  await ffmpegInstance.run("-t", duration.toString(), "-f", "lavfi", "-i", "color=c=green:s=1280x720:r=30", "-f", "lavfi", "-i", "anullsrc=channel_layout=stereo:sample_rate=44100", "-pix_fmt", "yuv420p", "-shortest", "base.mp4");
 
   // sort clips by z index, lowest to highest. we do this so we properly layer the videos.
   clips = clips.sort((a, b) => a.z - b.z);
@@ -39,6 +39,16 @@ export const exportVideo = async () => {
     const start = clip.start;
     const end = (clip.media.duration - clip.end);
 
+    const trim = `duration=${end-start}`;
+    const setpts = `PTS-STARTPTS+${clip.offset-clip.start}/TB`;
+    /**
+     * The portion of the ffmpeg filter that defines the scale of the clip.
+     * w/h represent the width and height of the clip, respectively; we multiply
+     * these by the clip's matrix[0] and matrix[3] values to get the scaled
+     * dimensions.
+     */
+    const scale = `(iw*${clip.matrix[0]}):(ih*${clip.matrix[3]})`;
+
     /**
      * trim=duration: trim the video so it doesn't go past clip.end (otherwise,
      * it video will keep playing as if video was still playing, despite it not
@@ -47,8 +57,11 @@ export const exportVideo = async () => {
      * setpts=PTS-STARTPTS+delay: resync video presentation timestamp to start
      * such that the video's start is "delayed" by the clip.start when it
      * starts playing
+     *
+     * scale=width:height: scale the video to the clip's dimensions. we do this
+     * here since there is only one input link.
      */
-    vfilter += `[${i}:v]trim=duration=${end-start},setpts=PTS-STARTPTS+${clip.offset-clip.start}/TB[${i}v];`
+    vfilter += `[${i}:v]trim=${trim},setpts=${setpts},scale=${scale}[${i}v];`
 
     // define delay, since we need it twice
     const d = (clip.offset * 1000).toFixed(0);
@@ -77,11 +90,21 @@ export const exportVideo = async () => {
     const clip = clips[i];
 
     /**
-     * overlay=
-     *  - (W-w)/2:(H-h)/2: center the video within the frame
-     *  - enable='between(t,offset,offset+duration)': only show the video when it's supposed to be shown
+     * The portion of the ffmpeg filter that defines the position of the clip.
+     * (W-w)/2 and (H-h)/2 are used to center the video; we offset these by the
+     * clip's matrix[4] and matrix[5] values.
      */
-    vfilter += `${inLink}[${i + 1}v]overlay=(W-w)/2:(H-h)/2:enable='between(t\\,${clip.offset},${clip.offset + (clip.media.duration - clip.end - clip.start)})'${outLink};`;
+    const overlayPos = `(W-w)/2+${clip.matrix[4]}:(H-h)/2+${clip.matrix[5]}`;
+
+    /**
+     * The portion of the ffmpeg filter that defines the period over which the
+     * clip is enabled. This starts at clip.offset, and lasts until the the end
+     * of the clip (in absolute positioning: offset + calculated duration).
+     */
+    const enabledPeriod = `enable='between(t\\,${clip.offset},${clip.offset + (clip.media.duration - clip.end - clip.start)})'`;
+
+    //overlay=<overlayW>:<overlayH>:<enabledPeriod>
+    vfilter += `${inLink}[${i + 1}v]overlay=${overlayPos}:${enabledPeriod}${outLink};`
   }
 
   // -----------------------
