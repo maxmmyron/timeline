76c0b981108a58365981e8027174f4d6f4b3bc7d
maxmmyron <41480159+maxmmyron@users.noreply.github.com>
2024-02-02 19:02:54 -0500

    implement audioContext as audio-thing-doer

---
+++ src/lib/components/TimelineMedia.svelte
@@ -0,0 +1,56 @@
+<script lang="ts">
+  import { aCtx, vRefs, aRefs, playerScale } from "$lib/stores";
+  import { onMount } from "svelte";
+
+  export let clip: App.Clip;
+
+  export let curr: App.Clip[] = [];
+
+  let sourceNode: MediaElementAudioSourceNode;
+  let gainNode: GainNode = $aCtx!.createGain();
+  let panNode: StereoPannerNode = $aCtx!.createStereoPanner();
+
+  $: gainNode.gain.value = clip.volume;
+
+  onMount(() => {
+    if (clip.media.type === "video") {
+      sourceNode = $aCtx!.createMediaElementSource($vRefs[clip.uuid]);
+    } else {
+      sourceNode = $aCtx!.createMediaElementSource($aRefs[clip.uuid]);
+    }
+
+    sourceNode.connect(gainNode);
+    gainNode.connect(panNode);
+    panNode.connect($aCtx!.destination);
+
+    return () => {
+      sourceNode.disconnect();
+      gainNode.disconnect();
+      panNode.disconnect();
+    };
+  });
+</script>
+
+{#if clip.media.type === "video"}
+  <video
+    class="absolute top-1/2 left-1/2"
+    src={clip.media.src}
+    title={clip.uuid}
+    bind:this={$vRefs[clip.uuid]}
+    style:transform="translate(-50%, -50%) matrix({clip.matrix
+      .map((m, i) => (i === 0 || i == 3 ? m * $playerScale : m))
+      .join(",")})"
+    style:z-index={clip.z}
+    class:hidden={curr.findIndex((c) => c.uuid === clip.uuid) === -1}
+    preload=""
+  >
+    <track kind="captions" />
+  </video>
+{:else if clip.media.type === "audio"}
+  <audio
+    class="hidden"
+    src={clip.media.src}
+    title={clip.uuid}
+    bind:this={$aRefs[clip.uuid]}
+  />
+{/if}
