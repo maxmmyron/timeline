05bc7ba2172623eb72b17e824b9501d29d6e9dad
maxmmyron <41480159+maxmmyron@users.noreply.github.com>
2024-02-07 11:06:14 -0500

    improve styling; add visual link icon (logic WIP)

--- src/lib/components/Inspector/Inspector.svelte
+++ src/lib/components/Inspector/Inspector.svelte
@@ -41,61 +41,99 @@
 
   {#if type === "video" || type === "image"}
     <section
-      class="pb-2 mb-2 border-b border-zinc-300 dark:border-zinc-800 space-y-3"
+      class="pb-2 mb-2 border-b border-zinc-300 dark:border-zinc-800 space-y-3 grid grid-cols-[1fr,24px] gap-y-2 w-full"
     >
-      <header class="flex justify-between">
-        <h2 class="text-sm font-mono">Transforms</h2>
-        <button
-          class="bg-zinc-800 p-1 h-5 rounded-md shadow-md flex flex-col items-center justify-center border border-zinc-700"
-          on:click={() => (matrix = [1, 0, 0, 1, 0, 0])}>↻</button
-        >
+      <header class="col-start-1 grid grid-cols-subgrid">
+        <div class="flex justify-between">
+          <h2 class="text-sm font-mono">Transforms</h2>
+          <button
+            class="bg-zinc-800 p-1 h-5 rounded-md shadow-md flex flex-col items-center justify-center border border-zinc-700"
+            on:click={() => (matrix = [1, 0, 0, 1, 0, 0])}>↻</button
+          >
+        </div>
       </header>
 
-      <section class="space-y-2">
-        <p>Scale</p>
+      <section class="col-span-full grid grid-cols-subgrid gap-2">
+        <p class="col-start-1">Scale</p>
         <ScalarSetting
+          class="col-start-1"
           name="X"
           bind:scalar={matrix[0]}
           props={{ min: 0, max: 6, step: 0.01 }}
         />
         <ScalarSetting
+          class="col-start-1"
           name="Y"
           bind:scalar={matrix[3]}
           props={{ min: 0, max: 6, step: 0.01 }}
         />
+        <div
+          class="col-start-2 row-start-2 row-span-2 flex flex-col justify-center gap-1"
+        >
+          <div
+            class="w-1/2 h-2 rounded-tr-md border-t border-r border-dashed border-zinc-700"
+          />
+          <button
+            class="bg-zinc-800 p-1 h-5 rounded-md shadow-md flex flex-col items-center justify-center border border-zinc-700"
+          >
+            🔗
+          </button>
+          <div
+            class="w-1/2 h-2 rounded-br-md border-b border-r border-dashed border-zinc-700"
+          />
+        </div>
       </section>
 
-      <section class="space-y-2">
+      <section class="col-span-full grid grid-cols-subgrid gap-2">
         <p>Translate</p>
         <ScalarSetting
+          class="col-start-1"
           name="X"
           bind:scalar={matrix[4]}
           props={{ min: -$safeRes[0] / 2, max: $safeRes[0] / 2, step: 0.01 }}
         />
         <ScalarSetting
           name="Y"
+          class="col-start-1"
           bind:scalar={matrix[5]}
           props={{ min: -$safeRes[1] / 2, max: $safeRes[1] / 2, step: 0.01 }}
         />
+        <div
+          class="col-start-2 row-start-2 row-span-2 flex flex-col justify-center gap-1"
+        >
+          <div
+            class="w-1/2 h-2 rounded-tr-md border-t border-r border-dashed border-zinc-700"
+          />
+          <button
+            class="bg-zinc-800 p-1 h-5 rounded-md shadow-md flex flex-col items-center justify-center border border-zinc-700"
+          >
+            🔗
+          </button>
+          <div
+            class="w-1/2 h-2 rounded-br-md border-b border-r border-dashed border-zinc-700"
+          />
+        </div>
       </section>
     </section>
   {/if}
   {#if type !== "image"}
     <section
-      class="pb-2 mb-2 border-b border-zinc-300 dark:border-zinc-800 space-y-3"
+      class="pb-2 mb-2 border-b border-zinc-300 dark:border-zinc-800 space-y-3 grid grid-cols-[1fr,24px] gap-1"
     >
-      <header class="flex justify-between">
-        <h2 class="text-sm font-mono">Audio</h2>
-        <button
-          class="bg-zinc-800 p-1 h-5 rounded-md shadow-md flex flex-col items-center justify-center border border-zinc-700"
-          on:click={() => {
-            volume = 1;
-            pan = 0;
-          }}>↻</button
-        >
+      <header class="col-start-1 grid grid-cols-subgrid">
+        <div class="flex justify-between">
+          <h2 class="text-sm font-mono">Audio</h2>
+          <button
+            class="bg-zinc-800 p-1 h-5 rounded-md shadow-md flex flex-col items-center justify-center border border-zinc-700"
+            on:click={() => {
+              volume = 1;
+              pan = 0;
+            }}>↻</button
+          >
+        </div>
       </header>
 
-      <section class="space-y-2">
+      <section class="col-start-1 space-y-2">
         <ScalarSetting
           name="Volume"
           bind:scalar={volume}
