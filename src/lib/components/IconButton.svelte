a979f0b3abc2ca01b6e1c35e97bc88c9a24141e2
maxmmyron <41480159+maxmmyron@users.noreply.github.com>
2024-02-12 00:51:21 -0500

    add light-mode support

--- src/lib/components/IconButton.svelte
+++ src/lib/components/IconButton.svelte
@@ -4,7 +4,7 @@
 
 <button
   on:click
-  class="w-5 h-5 flex items-center justify-center bg-zinc-800 border-t border-t-zinc-500 shadow-sm rounded-md"
+  class="w-5 h-5 flex items-center justify-center bg-zinc-200 dark:bg-zinc-800 border-t border-t-zinc-100 dark:border-t-zinc-500 shadow-sm rounded-md"
   aria-label={alt}
   {...$$restProps}
 >
