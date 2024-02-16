fafd969d4413e4101d9f65eca3fdd463ee4fa1ca
maxmmyron <41480159+maxmmyron@users.noreply.github.com>
2024-02-15 23:29:48 -0500

    add toggle state

--- src/lib/components/IconButton.svelte
+++ src/lib/components/IconButton.svelte
@@ -1,12 +1,20 @@
 <script lang="ts">
   export let alt: string;
+  export let toggles: boolean = false;
+
+  let toggled = false;
 </script>
 
 <button
   on:click
-  class="w-5 h-5 flex items-center justify-center bg-zinc-200 dark:bg-zinc-800 border-t border-t-zinc-100 dark:border-t-zinc-500 shadow-sm rounded-md"
+  class="w-5 h-5 flex items-center justify-center bg-zinc-200 dark:bg-zinc-800 border-t border-t-zinc-100 dark:border-t-zinc-500 shadow-sm rounded-md {toggled
+    ? 'bg-zinc-300 dark:bg-zinc-900 border-t-zinc-200 dark:border-t-zinc-700'
+    : ''}"
   aria-label={alt}
   {...$$restProps}
+  on:click={() => {
+    if (toggles) toggled = !toggled;
+  }}
 >
   <slot />
 </button>
