306a0edc2612059f68291217239c75a747dec5ba
Max Myron <max@mmyron.com>
2024-06-12 16:42:05 -0400

    add unimplemented tests for loader functions

--- src/test/unit/lib/loader.test.ts
+++ src/test/unit/lib/loader.test.ts
@@ -4,3 +4,41 @@ import { test } from "vitest"
 test("returns a wrapped promise when given a file", () => {
 
 });
+
+// resolveMedia
+
+test("unknown MIME type rejects", () => {});
+
+test("video/mp4 resolves to video media", () => {});
+
+test("video/ogg resolves to video media", () => {});
+
+test("video/webm resolves to video media", () => {});
+
+test("other video MIME type rejects", () => {});
+
+test("audio/mpeg resolves to audio media", () => {});
+
+test("audio/ogg resolves to audio media", () => {});
+
+test("audio/wav resolves to audio media", () => {});
+
+test("other audio MIME type rejects", () => {});
+
+test("image/jpeg resolves to image media", () => {});
+
+test("image/png resolves to image media", () => {});
+
+test("other image MIME type rejects", () => {});
+
+// canMediaPlay
+
+// TODO: this may depend on the browser we're mocking?
+
+// resolveDuration
+
+test("video file has duration", () => {});
+
+test("audio file has duration", () => {});
+
+test("image file does not have duration", () => {});
