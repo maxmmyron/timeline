f95dd96ca36339648d5e0dcb10d6e8b42e58f9bf
maxmmyron <41480159+maxmmyron@users.noreply.github.com>
2024-06-12 20:30:23 -0400

    add initial tests, fix testing path

--- src/test/unit/lib/loader.test.ts
+++ src/test/unit/lib/loader.test.ts
@@ -1,44 +1,128 @@
 import { createMediaFromFile, resolveMedia, canMediaPlay, resolveDuration, resolveDimensions } from "$lib/loader";
-import { test } from "vitest"
+import { beforeEach, describe, expect, it, test, vi } from "vitest"
 
-test("returns a wrapped promise when given a file", () => {
+describe("createMediaFromFile", () => {
+  beforeEach(() => {
+    global.URL.createObjectURL = vi.fn();
+  });
 
+  it("returns a wrapped promise when given a file", () => {
+    const media = createMediaFromFile(new File([], "test.jpg", {type: "image/jpeg"}));
+
+    expect(media).toHaveProperty("uuid");
+    expect(media).toHaveProperty("title");
+    expect(media).toHaveProperty("media");
+    expect(media.media).toBeInstanceOf(Promise);
+  });
 });
 
 // resolveMedia
 
-test("unknown MIME type rejects", () => {});
+describe("resolveMedia", () => {
+  beforeEach(() => {
+    global.URL.createObjectURL = vi.fn();
+  });
 
-test("video/mp4 resolves to video media", () => {});
+  it("rejects if the MIME type is empty", () => {
+    const file = new File([], "test.jpg", {type: ""});
+    expect(resolveMedia(file, "test")).rejects.toEqual("Unsupported file type.");
+  });
 
-test("video/ogg resolves to video media", () => {});
+  it("rejects if the MIME type is not a known media type", () => {
+    const file = new File([], "test.jpg", {type: "application/pdf"});
+    expect(resolveMedia(file, "test")).rejects.toEqual("Unsupported file type.");
+  });
 
-test("video/webm resolves to video media", () => {});
+  it("rejects if the MIME type is not supported by the browser", () => {
+    const file = new File([], "test.jpg", {type: "image/svg+xml"});
+    expect(resolveMedia(file, "test")).rejects.toEqual("Unsupported file type.");
+  });
 
-test("other video MIME type rejects", () => {});
+  it("resolves to a media object with the correct properties", () => {
+    const file = new File([], "test.jpg", {type: "image/jpeg"});
+    const media = resolveMedia(file, "test");
 
-test("audio/mpeg resolves to audio media", () => {});
+    expect(media).resolves.toEqual({
+      uuid: "test",
+      src: expect.any(String),
+      duration: expect.any(Number),
+      title: "test.jpg",
+      dimensions: expect.any(Object),
+      type: "image"
+    });
+  });
+});
 
-test("audio/ogg resolves to audio media", () => {});
+// canMediaPlay (TODO: this may depend on the browser)
+describe("canMediaPlay", () => {
+  beforeEach(() => {
+    global.document.createElement = vi.fn();
+  });
 
-test("audio/wav resolves to audio media", () => {});
+  it("returns true for a supported video type", () => {
+    const file = new File([], "test.mp4", {type: "video/mp4"});
+    expect(canMediaPlay(file)).resolves.toBe(true);
 
-test("other audio MIME type rejects", () => {});
+    const file2 = new File([], "test.ogv", {type: "video/ogg"});
+    expect(canMediaPlay(file2)).resolves.toBe(true);
 
-test("image/jpeg resolves to image media", () => {});
+    const file3 = new File([], "test.webm", {type: "video/webm"});
+    expect(canMediaPlay(file3)).resolves.toBe(true);
+  });
 
-test("image/png resolves to image media", () => {});
+  it("returns true for a supported audio type", () => {
+    const file = new File([], "test.mp3", {type: "audio/mpeg"});
+    expect(canMediaPlay(file)).resolves.toBe(true);
 
-test("other image MIME type rejects", () => {});
+    const file2 = new File([], "test.ogg", {type: "audio/ogg"});
+    expect(canMediaPlay(file2)).resolves.toBe(true);
 
-// canMediaPlay
+    const file3 = new File([], "test.wav", {type: "audio/wav"});
+    expect(canMediaPlay(file3)).resolves.toBe(true);
+  });
 
-// TODO: this may depend on the browser we're mocking?
+  it("returns true for a supported image type", () => {
+    const file = new File([], "test.jpg", {type: "image/jpeg"});
+    expect(canMediaPlay(file)).resolves.toBe(true);
+
+    const file2 = new File([], "test.png", {type: "image/png"});
+    expect(canMediaPlay(file2)).resolves.toBe(true);
+  });
+
+  it("returns false for an unsupported type", () => {
+    const file = new File([], "test.svg", {type: "image/svg+xml"});
+    expect(canMediaPlay(file)).resolves.toBe(false);
+
+    const file2 = new File([], "test.pdf", {type: "application/pdf"});
+    expect(canMediaPlay(file2)).resolves.toBe(false);
+
+    const file3 = new File([], "test.doc", {type: "application/msword"});
+    expect(canMediaPlay(file3)).resolves.toBe(false);
+  });
+});
 
 // resolveDuration
 
-test("video file has duration", () => {});
+describe("resolveDuration", () => {
+  beforeEach(() => {
+    global.document.createElement = vi.fn();
+  });
 
-test("audio file has duration", () => {});
+  it("video file has duration", () => {
+    const src = "test.mp4";
+    const mime = "video";
+    expect(resolveDuration(src, mime)).resolves.toBeGreaterThan(0);
+  });
 
-test("image file does not have duration", () => {});
+  it("audio file has duration", () => {
+    const src = "test.mp3";
+    const mime = "audio";
+    expect(resolveDuration(src, mime)).resolves.toBeGreaterThan(0);
+  });
+
+  it("image file does not have duration", () => {
+    const src = "test.jpg";
+    const mime = "image";
+    expect(resolveDuration(src, mime)).resolves.toBe(0);
+  });
+});
--- vite.config.js
+++ vite.config.js
@@ -16,6 +16,6 @@ export default defineConfig({
 		}
 	},
 	"test": {
-		include: ["/test/**/*.test.{js,ts}"]
+		include: ["src/test/**/*.test.{js,ts}"]
 	}
 });
