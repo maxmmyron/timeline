affafff8c64189118a167f0d69e8790e66464ec9
maxmmyron <41480159+maxmmyron@users.noreply.github.com>
2024-06-22 18:57:56 -0400

    don't show panel if no clip selected

--- src/lib/components/Panel/NodePanel.svelte
+++ src/lib/components/Panel/NodePanel.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import { selected } from "$lib/stores";
   import { spring } from "svelte/motion";
 
   let offset = spring([0, 0], { stiffness: 0.1, damping: 0.3 });
@@ -28,15 +29,21 @@
   on:mousemove={(e) => move(e.clientX, e.clientY)}
 />
 
-<div
-  on:dblclick={() => offset.set([0, 0])}
-  on:mousedown={(e) => startMove(e.clientX, e.clientY)}
-  on:touchstart={(e) => startMove(e.touches[0].clientX, e.touches[0].clientY)}
-  role="grid"
-  tabindex="0"
-  class="relative bg-dot -top-1 -left-1 w-[calc(100%_+_.5rem)] h-[calc(100%_+_.5rem)] from-zinc-925 from-25% to-zinc-950 to-25%"
-  style="background-position: {$offset[0] % 8}px {$offset[1] % 8}px"
-></div>
+{#if $selected}
+  <div
+    on:dblclick={() => offset.set([0, 0])}
+    on:mousedown={(e) => startMove(e.clientX, e.clientY)}
+    on:touchstart={(e) => startMove(e.touches[0].clientX, e.touches[0].clientY)}
+    role="grid"
+    tabindex="0"
+    class="relative bg-dot -top-1 -left-1 w-[calc(100%_+_.5rem)] h-[calc(100%_+_.5rem)] from-zinc-925 from-25% to-zinc-950 to-25%"
+    style="background-position: {$offset[0] % 8}px {$offset[1] % 8}px"
+  ></div>
+{:else}
+  <div class="w-full h-full flex items-center justify-center">
+    <p>No clip selected</p>
+  </div>
+{/if}
 
 <style>
   .bg-dot {
